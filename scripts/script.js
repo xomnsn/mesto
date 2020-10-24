100402ea9907042421aa696804b260e922e503b2
Mariya Murasheva <xomnsn@gmail.com>
2020-10-24 13:21:51 +0300

    feature: add popup markup and sample functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,53 @@
 const page = document.querySelector('.page')
-const editBtn = page.querySelector('.profile__edit-button');
+const editUserBtn = page.querySelector('.profile__edit-button');
+const addPlaceBtn = page.querySelector('.profile__add-button');
 const profileName = page.querySelector('.profile__name');
 const profileDescription = page.querySelector('.profile__description');
-const popup = page.querySelector('.pop-up');
-const popupCloseBtn = popup.querySelector('.pop-up__close-button');
-const form = popup.querySelector('.pop-up__form');
-const popupNameInput = form.querySelector('.pop-up__text-input_type_name');
-const popupDescriptionInput = form.querySelector('.pop-up__text-input_type_description');
+
+const editUserPopup = page.querySelector('.pop-up_func_edit');
+const editUserCLoseBtn = editUserPopup.querySelector('.pop-up__close-button');
+const editUserForm = editUserPopup.querySelector('.pop-up__form');
+const editUserNameInput = editUserForm.querySelector('.pop-up__text-input_type_name');
+const editUserDescriptionInput = editUserForm.querySelector('.pop-up__text-input_type_description');
+
+const addPlacePopup = page.querySelector('.pop-up_func_add');
+const addPlaceCLoseBtn = addPlacePopup.querySelector('.pop-up__close-button');
+const addPlaceForm = addPlacePopup.querySelector('.pop-up__form');
+const addPlaceTitleInput = addPlacePopup.querySelector('.pop-up__text-input_type_title');
+const addPlaceSrcInput = addPlacePopup.querySelector('.pop-up__text-input_type_src');
 
 function onEditBtn() {
-  popupNameInput.value = profileName.textContent;
-  popupDescriptionInput.value = profileDescription.textContent;
-  popup.classList.add('pop-up_opened');
+  editUserNameInput.value = profileName.textContent;
+  editUserDescriptionInput.value = profileDescription.textContent;
+  editUserPopup.classList.add('pop-up_opened');
 }
 
-function onCloseBtn() {
+function onAddBtn() {
+  addPlacePopup.classList.add('pop-up_opened');
+}
+
+function onClosePopup(evt) {
+  const popup = evt.target.closest('.pop-up')
   popup.classList.remove('pop-up_opened');
 }
 
-function formSubmitHandler (evt) {
+function editUserSubmitHandler (evt) {
   evt.preventDefault();
-  profileName.textContent = popupNameInput.value;
-  profileDescription.textContent = popupDescriptionInput.value;
-  onCloseBtn();
+  profileName.textContent = editUserNameInput.value;
+  profileDescription.textContent = editUserDescriptionInput.value;
+  onClosePopup(evt);
 }
 
-editBtn.addEventListener('click', onEditBtn);
-popupCloseBtn.addEventListener('click', onCloseBtn);
-form.addEventListener('submit', formSubmitHandler);
+function addPlaceSubmitHandler (evt) {
+  evt.preventDefault();
+
+  onClosePopup(evt);
+}
+
+editUserBtn.addEventListener('click', onEditBtn);
+editUserCLoseBtn.addEventListener('click', onClosePopup);
+editUserForm.addEventListener('submit', editUserSubmitHandler);
+
+addPlaceBtn.addEventListener('click', onAddBtn);
+addPlaceCLoseBtn.addEventListener('click', onClosePopup);
+addPlaceForm.addEventListener('submit', addPlaceSubmitHandler);
