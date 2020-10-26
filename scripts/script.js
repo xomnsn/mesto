222d36502c2103c0ac3249b0e683964038a3ccde
Mariya Murasheva <xomnsn@gmail.com>
2020-10-26 18:39:26 +0300

    hotfix: fix pop-up fade-out

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,3 @@
-  const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ];
-
   const page = document.querySelector('.page')
   const editUserBtn = page.querySelector('.profile__edit-button');
   const addPlaceBtn = page.querySelector('.profile__add-button');
@@ -84,10 +57,7 @@
 
   function closePopup(evt) {
     const popup = evt.target.closest('.pop-up')
-    popup.classList.add('pop-up_closing');
-    setTimeout(() =>
-      popup.classList.remove('pop-up_opened', 'pop-up_closing'),
-      400);
+    popup.classList.remove('pop-up_opened');
   }
 
   function editUserSubmitHandler (evt) {
